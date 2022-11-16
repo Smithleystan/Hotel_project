34f96fc8f862b2f26482af60f507faa3b49167b8
stan <stan.smithley@gmail.com>
2022-11-16 14:39:43 +0100

    test

--- app.js
+++ app.js
@@ -101,6 +101,7 @@ do{
 
                                     }else if(answer == "non"){
                                         console.log('annulation')
+                                        
                                         rl.close()
                                     }
                                 })
